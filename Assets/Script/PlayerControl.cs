40199b438ba9cff2538cbd641d3eb912cea149ee
Aosagi3293 <anemone20050410@gmail.com>
2026-07-11 17:20:52 +0900

    作業開始

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = this.gameObject;
+    }
+
+    
+}
